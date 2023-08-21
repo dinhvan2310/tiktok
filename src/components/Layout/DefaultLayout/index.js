4eecfb09a4f9f98f4de9ad05cd10be97d6fc7575
Paimom <trandinhvan0294@gmail.com>
2023-08-21 16:41:14 +0700

    header2

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,16 @@
 import Header from 'components/Layout/components/Header';
 import Sidebar from './Sidebar';
+import styles from 'components/Layout/DefaultLayout/DefaultLayout.module.scss';
+
+import { clsx } from 'clsx';
 
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={clsx(styles.wrapper)}>
       <Header />
-      <div className="container">
+      <div className={clsx(styles.container)}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={clsx(styles.content)}>{children}</div>
       </div>
     </div>
   );
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,35 @@
+import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
+
+import { logo } from 'assets/images';
+import styles from './Header.module.scss';
+
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={clsx(styles.wrapper)}>
+      <div className={clsx(styles.inner)}>
+        <img src={logo} alt="Tiktok" />
+        <Tippy content="Search">
+          <div className={clsx(styles.search)}>
+            <input placeholder="Search accounts and videos" spellCheck={false} />
+            <button className={clsx(styles.clear)}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={clsx(styles.loading)} icon={faSpinner} />
+
+            <button className={clsx(styles['search-btn'])}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+        <div className={clsx(styles.actions)}></div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
